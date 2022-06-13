681593b14ad0c44c691006c6e863c6619ea789bf
miles <miles@greatcactus.org>
2022-06-13 12:33:33 +1000

    Created basic UI, but no functionality yet.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-
+#include <iostream>
 int main(int argc, char *argv[])
 {
+    std::cout<<"QT version is "<<QT_VERSION_MAJOR<<"."<<QT_VERSION_MINOR<<std::endl;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();
+    return QApplication::exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <iostream>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->answer_button, &QPushButton::pressed, [=](){
+        std::cout<<"Button pressed"<<std::endl;
+    });
+    connect(ui->answer_input, &QLineEdit::returnPressed, ui->answer_button, &QPushButton::pressed);
 }
 
 MainWindow::~MainWindow()
