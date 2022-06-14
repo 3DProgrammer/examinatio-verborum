b652b3a2e0904ae3d1deacbf8174d50bb3992fc9
3DProgrammer <78192215+3DProgrammer@users.noreply.github.com>
2022-06-14 19:21:50 +1000

    Added versioning.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 #include <iostream>
 int main(int argc, char *argv[])
 {
+    srand(time(nullptr));
     std::cout<<"QT version is "<<QT_VERSION_MAJOR<<"."<<QT_VERSION_MINOR<<std::endl;
+    std::cout<<"Examinatio verborum version is "<<PROJECT_VERSION_MAJOR<<"."<<PROJECT_VERSION_MINOR<<std::endl;
+    auto styles = QStyleFactory::keys();
+    std::cout<<styles.size()<<" styles available."<<std::endl;
+    for (const auto& style:styles) {
+        std::cout<<style.toStdString()<<std::endl;
+    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
