a7e04f8f1aa925ace5985821ba3bc2cd33a1dd75
3DProgrammer <78192215+3DProgrammer@users.noreply.github.com>
2022-06-16 14:28:40 +1000

    More wordlist parsing.

--- wordlistParser.cpp
+++ wordlistParser.cpp
@@ -5,78 +5,72 @@
 #include "wordlistParser.h"
 #include <iostream>
 #include <fstream>
-std::vector<std::string> split(const std::string& line, char splitter, char quotes) {
+std::map<std::string,bibliotheca::Gender> WordList::stringToGender={{"m",bibliotheca::Gender::m},{"f",bibliotheca::Gender::f},{"n",bibliotheca::Gender::n}};
+std::map<std::string,int> WordList::stringToDeclension={{"1",1},{"2",2},{"3",3},{"4",4},{"5",5}};
+std::vector<std::string> split(const std::string &line, char splitter, char quotes) {
     std::vector<std::string> result;
     if (line.empty()) {
         return result;
     }
-    int last_index=0;
-    bool quoting=false;
+    int last_index = 0;
+    bool quoting = false;
     for (int i = 0; i < line.size(); ++i) {
-        if (line[i]==quotes) {
+        if (line[i] == quotes) {
             if (quoting) {
-                quoting=false;
-                if(i>last_index) {
-                    result.push_back(line.substr(last_index, i-last_index));
+                quoting = false;
+                if (i > last_index) {
+                    result.push_back(line.substr(last_index, i - last_index));
                 }
-                last_index=i+1;
-            }
-            else {
-                quoting=true;
-                if (i>last_index) {
-                    result.push_back(line.substr(last_index, i-last_index));
+                last_index = i + 1;
+            } else {
+                quoting = true;
+                if (i > last_index) {
+                    result.push_back(line.substr(last_index, i - last_index));
                 }
-                last_index=i+1;
+                last_index = i + 1;
             }
         }
-        if (line[i]==splitter&&!quoting) {
-            if (i>last_index) {
-                result.push_back(line.substr(last_index, i-last_index));
+        if (line[i] == splitter && !quoting) {
+            if (i > last_index) {
+                result.push_back(line.substr(last_index, i - last_index));
             }
-            last_index=i+1;
+            last_index = i + 1;
         }
     }
-    if (line.size()>last_index) {
-        result.push_back(line.substr(last_index, line.size()-last_index));
+    if (line.size() > last_index) {
+        result.push_back(line.substr(last_index, line.size() - last_index));
     }
     return result;
 }
-WordList::WordList(const std::string &fileName) {
+
+WordList::WordList(const std::string &fileName) { // TODO: Error checking for user input and non-ASCII support.
     std::ifstream infile(fileName);
     std::string line;
     getline(infile, line);
-    std::string versionCheck=line;
+    std::string versionCheck = line;
     versionCheck.resize(7);
-    if (versionCheck!="version") {
-        std::cerr<<"No version declaration found."<<std::endl;
-    }
-    else {
-        std::string versionNum=line;
-        versionNum.erase(0,8);
-        if (versionNum=="1") {
-            while (getline(infile,line)) {
-                std::cout<<"Got new line: \""<<line<<"\""<<std::endl;
-                std::string lineType=line;
-                auto lineSplit= split(line, ' ', '"');
-                for (int i = 0; i < lineType.size(); ++i) {
-                    if (lineType[i]==' ') {
-                        lineType.erase(i, lineType.size());
-                        break;
-                    }
-                }
-                if (lineType=="name") {
-                    std::cout<<"Setting word list name."<<std::endl;
-                }
-                else if (lineType=="N") {
-                    std::cout<<"Inputting new noun."<<std::endl;
-                }
-                else {
-                    std::cerr<<"Unrecognised input type \""<<lineType<<"\""<<std::endl;
+    if (versionCheck != "version") {
+        std::cerr << "No version declaration found." << std::endl;
+    } else {
+        std::string versionNum = line;
+        versionNum.erase(0, 8);
+        if (versionNum == "1") {
+            while (getline(infile, line)) {
+                std::cout << "Got new line: \"" << line << "\"" << std::endl;
+                auto lineSplit = split(line, ' ', '"');
+                std::string lineType = lineSplit[0];
+                if (lineType == "name") {
+                    std::cout << "Setting word list name." << std::endl;
+                    name = lineSplit[1];
+                } else if (lineType == "N") {
+                    std::cout << "Inputting new noun." << std::endl;
+                    nouns.emplace_back(lineSplit[1], lineSplit[2], stringToGender[lineSplit[3]], split(lineSplit[5], ';', '\''), stringToDeclension[lineSplit[4]]);
+                } else {
+                    std::cerr << "Unrecognised input type \"" << lineType << "\"" << std::endl;
                 }
             }
-        }
-        else {
-            std::cerr<<versionNum<<" is not a valid version."<<std::endl;
+        } else {
+            std::cerr << versionNum << " is not a valid version." << std::endl;
         }
     }
 }
--- wordlistParser.h
+++ wordlistParser.h
@@ -6,10 +6,14 @@
 #define EXAMINATIO_VERBORUM_WORDLISTPARSER_H
 #include <vector>
 #include <bibliotheca.h>
+#include <map>
 class WordList {
 public:
     std::vector<bibliotheca::Noun> nouns;
+    std::string name;
     explicit WordList(const std::string& fileName);
     WordList();
+    static std::map<std::string,bibliotheca::Gender> stringToGender;
+    static std::map<std::string,int> stringToDeclension;
 };
 #endif //EXAMINATIO_VERBORUM_WORDLISTPARSER_H
