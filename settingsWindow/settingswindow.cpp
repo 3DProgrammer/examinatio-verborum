47660c21bb7177e5612e9283fe10bebc03d3dfab
3DProgrammer <78192215+3DProgrammer@users.noreply.github.com>
2022-06-29 09:27:46 +1000

    More settings.

--- settingsWindow/settingswindow.cpp
+++ settingsWindow/settingswindow.cpp
@@ -8,10 +8,38 @@
 #include <QStyleFactory>
 #include <iostream>
 #include <QFontDialog>
-
+#define addSetting(name, signal, setter, argType) {QSettings settings; if (settings.contains(QString::fromStdString(std::string("AUTO_SETTING")+#name))) {ui->name->setter(settings.value(QString::fromStdString(std::string("AUTO_SETTING")+#name)).value<argType>());} connect(ui->name, &std::remove_pointer<decltype(ui->name)>::type::signal, [=](argType value){std::cout<<#name<<" set to "<<value<<std::endl;QSettings newSettings;newSettings.setValue(QString::fromStdString(std::string("AUTO_SETTING")+#name),value);});}
 
 SettingsWindow::SettingsWindow(QWidget *parent) : QWidget(parent), ui(new Ui::SettingsWindow) {
     ui->setupUi(this);
+
+    addSetting(enable_nouns, stateChanged, setChecked, bool);
+    addSetting(n_w_sing, valueChanged, setValue, int);
+    addSetting(n_w_plur, valueChanged, setValue, int);
+    addSetting(n_w_nom, valueChanged, setValue, int);
+    addSetting(n_w_acc, valueChanged, setValue, int);
+    addSetting(n_w_gen, valueChanged, setValue, int);
+    addSetting(n_w_dat, valueChanged, setValue, int);
+    addSetting(n_w_abl, valueChanged, setValue, int);
+    addSetting(n_w_voc, valueChanged, setValue, int);
+    addSetting(enable_verbs, stateChanged, setChecked, bool);
+    addSetting(v_w_s, valueChanged, setValue, int);
+    addSetting(v_w_p, valueChanged, setValue, int);
+    addSetting(v_w_1, valueChanged, setValue, int);
+    addSetting(v_w_2, valueChanged, setValue, int);
+    addSetting(v_w_3, valueChanged, setValue, int);
+    addSetting(v_w_ac, valueChanged, setValue, int);
+    addSetting(v_w_pa, valueChanged, setValue, int);
+    addSetting(v_w_inf, valueChanged, setValue, int);
+    addSetting(v_w_imperative, valueChanged, setValue, int);
+    addSetting(v_w_ind, valueChanged, setValue, int);
+    addSetting(v_w_sub, valueChanged, setValue, int);
+    addSetting(v_w_plu, valueChanged, setValue, int);
+    addSetting(v_w_per, valueChanged, setValue, int);
+    addSetting(v_w_imperfect, valueChanged, setValue, int);
+    addSetting(v_w_pre, valueChanged, setValue, int);
+    addSetting(v_w_fut, valueChanged, setValue, int);
+    addSetting(v_w_futper, valueChanged, setValue, int);
     auto styles = QStyleFactory::keys();
     qsizetype numStyles=styles.size();
     std::cout<<styles.size()<<" styles available."<<std::endl;
